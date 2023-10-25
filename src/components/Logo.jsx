ea8099e126d03c97710e41d44befb00b7f0941b6
isaiasd2712 <isaiasd2712@gmail.com>
2023-10-25 20:35:04 +0200

    header fase1

---
+++ src/components/Logo.jsx
@@ -0,0 +1,8 @@
+import Logo from '../assets/logo-deca.png'
+
+function Logo() {
+    return (
+        <img src={Logo} alt="Mi página"/>
+    );
+}
+export default Logo;
---
+++ src/components/body.jsx
@@ -0,0 +1,11 @@
+import './body.css'
+import Carrusel from '../assets/Carrusel.png'
+
+function body (){
+    return 
+    <img src={Carrusel} alt='uncarrusel'></img>
+    
+      
+}
+
+export default body 
